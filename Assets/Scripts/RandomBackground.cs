c8df78b9c4be82a2bba68246e85fc9063a365a7c
N1k0lenk0 <nikolenkogleb172@gmail.com>
2025-06-16 18:41:35 +0300

    Design changes
    Added design to the main menu, loading screen and game screen

---
+++ Assets/Scripts/RandomBackground.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class RandomBackgroundUI : MonoBehaviour
+{
+    public Image backgroundImage; // Сюди перетягуємо Image (UI)
+    private Sprite[] backgroundSprites;
+
+    void Start()
+    {
+        backgroundSprites = Resources.LoadAll<Sprite>("GameSceneObjects/BGs");
+
+        if (backgroundSprites.Length == 0)
+        {
+            Debug.LogError("Не знайдено спрайтів у Resources/InGameObjects/BGs");
+            return;
+        }
+
+        int index = Random.Range(0, backgroundSprites.Length-1);
+
+        //backgroundImage.sprite = backgroundSprites[index];
+        backgroundImage.sprite = backgroundSprites[3];
+    }
+}
